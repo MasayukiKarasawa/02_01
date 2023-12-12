eb84666e38e67e855ad586fc1a11353f48c53ed9
MasayukiKarasawa <115062911+MasayukiKarasawa@users.noreply.github.com>
2023-12-12 14:05:36 +0900

    接近、攻撃、離脱の表示

---
+++ Enemy.h
@@ -0,0 +1,35 @@
+#pragma once
+class Enemy
+{
+public:
+
+	void Update();
+
+	bool isGameLoop() { return GameLoop_; }
+
+private:
+
+	enum class Phase
+	{
+		kApproach,
+		kFire,
+		kLeave
+	};
+
+	//Ú‹ß
+	void Approach();
+	//UŒ‚
+	void Fire();
+	//—£’E
+	void Leave();
+
+	//ƒƒ“ƒo•Ï”
+	Phase phase_ = Phase::kApproach;
+
+	static void (Enemy::* FuncTable[])();
+
+	bool GameLoop_ = true;
+
+};
+
+
---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "Enemy.h"
+
+int main()
+{
+
+	Enemy enemy;
+
+	bool GameLoop = true;
+
+	int i = 0;
+
+	while (i < 3)
+	{
+		enemy.Update();
+		i++;
+	}
+
+	//GameLoop = enemy.isGameLoop();
+	//enemy.Update();
+	//enemy.Update();
+	//enemy.Update();
+
+	return 0;
+}
